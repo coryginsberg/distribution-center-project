a8506c55f556affb26359e5671dc85e21b60a93d
Cory Ginsberg <cory.ginsberg1@gmail.com>
2015-11-15 00:14:18 -0600

    Finally got Active Inventory to print properly. After some digging, I found out that I was switching the key and value for every Inventory for some reason, which is completely unnecessary. I fixed that and ran the program again, and it worked.

--- src/com/coryginsberg/GenerateFacilityStatusOutput.java
+++ src/com/coryginsberg/GenerateFacilityStatusOutput.java
@@ -6,9 +6,7 @@ import com.coryginsberg.managers.GraphManager;
 import com.coryginsberg.managers.InventoryManager;
 
 import java.nio.file.FileAlreadyExistsException;
-import java.util.*;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 
 /**
  * Outputs Each Facility's initial status and prints it out to the terminal.
@@ -63,9 +61,8 @@ public class GenerateFacilityStatusOutput implements OutputInterface {
             if (currentInventory.getCity().equals(network.getCity())) {
                 System.out.format("%-15s%-5s", "Item ID:", "Quantity:");
                 System.out.println();
-                System.out.format("%-15s%-5s", "‾‾‾‾‾‾‾‾‾", "‾‾‾‾‾‾‾‾‾‾");
+                System.out.format("%-15s%-5s", "‾‾‾‾‾‾‾‾‾", "‾‾‾‾‾‾‾‾‾");
                 System.out.println();
-                // TODO: 11/8/15 Get the 'Active Inventory' to print properly.
                 currentInventory.getNondepletedInventory().forEach((s, integer) -> {
 
                 });
@@ -87,56 +84,5 @@ public class GenerateFacilityStatusOutput implements OutputInterface {
         System.out.println();
         System.out.println("============================================================================================");
         System.out.println();
-    }
-}
-
-class Columns {
-
-    List<List<String>> lines = new ArrayList<>();
-    List<Integer> maxLengths = new ArrayList<>();
-    int numColumns = -1;
-
-    public Columns addLine(String... line) {
-
-        if (numColumns == -1){
-            numColumns = line.length;
-            for(int i = 0; i < numColumns; i++) {
-                maxLengths.add(0);
-            }
-        }
-
-        if (numColumns != line.length) {
-            throw new IllegalArgumentException();
-        }
-
-        for(int i = 0; i < numColumns; i++) {
-            maxLengths.set(  i, Math.max( maxLengths.get(i), line[i].length() )  );
-        }
-
-        lines.add( Arrays.asList(line) );
-
-        return this;
-    }
-
-    public void print(){
-        System.out.println( toString() );
-    }
-
-    public String toString(){
-        String result = "";
-        for(List<String> line : lines) {
-            for(int i = 0; i < numColumns; i++) {
-                result += pad( line.get(i), maxLengths.get(i) + 1 );
-            }
-            result += System.lineSeparator();
-        }
-        return result;
-    }
-
-    private String pad(String word, int newLength){
-        while (word.length() < newLength) {
-            word += " ";
-        }
-        return word;
     }
 }
